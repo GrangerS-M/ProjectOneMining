d8a044451a05032c511dfe90bf10522fbe03d969
Granger Smith-Massa <ges17@hampshire.edu>
2017-10-19 18:59:52 -0400

    Final Part

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour {
+
+	public string cubeType;
+
+	void OnMouseDown (){
+		Destroy (gameObject);
+
+		if (cubeType == "bronze"){
+			GameController.score += 1;
+			GameController.playerBronze -= 1;
+		}
+			
+		if (cubeType == "silver") {
+			GameController.score += 10;
+			GameController.playerSilver -= 1;
+		}
+
+		if (cubeType == "gold") {
+			GameController.score += 100;
+			GameController.playerGold -= 1;
+		}
+
+		print ("score:" + GameController.score);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	static public int playerBronze;
+	static public int playerSilver;
+	static public int playerGold;
+	// This integer, "mineGold", will ensure that only one gold is made when the proper requirements are met.
+	int mineGold;
+	int bronzeSupply;
+	int silverSupply;
+	int miningSpeed;
+	int timeToMine;
+	public GameObject CubePreFab;
+	GameObject currentCube;
+
+	static public int score;
+
+	Vector3 cubePosition;
+
+	// Use this for initialization
+	void Start () {
+		playerBronze = 0;
+		playerSilver = 0;
+		playerGold = 0;
+		miningSpeed = 2;
+		// Both "silverSupply" an "bronzeSupply" have been removed from use, as they only made things more complicated at this point. They do still exist, however.
+		mineGold = 1;
+		timeToMine = miningSpeed;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timeToMine) {
+
+			if (playerBronze == 2 && playerSilver == 2 && mineGold == 1) {
+				playerGold += 1;
+
+				//Create gold.
+				cubePosition = new Vector3 (Random.Range (-4, 5), Random.Range (-4, 5), 0);
+				currentCube = (GameObject)Instantiate (CubePreFab, cubePosition, Quaternion.identity);
+				currentCube.GetComponent <Renderer> ().material.color = Color.yellow;
+				currentCube.GetComponent <CubeBehavior>().cubeType = "gold";
+
+				mineGold = 0;
+			}
+
+			else if (playerBronze < 4) {
+				playerBronze += 1;
+
+				//Create silver.
+				cubePosition = new Vector3 (Random.Range (-4, 5), Random.Range (-4, 5), 0);
+				currentCube = (GameObject) Instantiate (CubePreFab, cubePosition, Quaternion.identity);
+				currentCube.GetComponent <Renderer>(). material.color = Color.red;
+				currentCube.GetComponent <CubeBehavior>().cubeType = "bronze";
+				// This line's placement ensures that gold will be only made once each time the other requirements are met.
+				mineGold = 1;
+			}
+			else if (playerBronze == 4) {
+				silverSupply -= 1;
+				playerSilver += 1;
+
+				//Create bronze.
+				cubePosition = new Vector3 (Random.Range (-4, 5), Random.Range (-4, 5), 0);
+				currentCube = (GameObject) Instantiate (CubePreFab, cubePosition, Quaternion.identity);
+				currentCube.GetComponent <Renderer>(). material.color = Color.white;
+				currentCube.GetComponent <CubeBehavior>().cubeType = "silver";
+				// This line's placement ensures that gold will be only made once each time the other requirements are met.
+				mineGold = 1;
+			}
+
+
+			timeToMine += miningSpeed;
+			// It was unclear in part 4 what was supposed to be done with this part of the code. I decided to keep it and add the player's gold to the printout.
+			print ("Bronze: " + playerBronze + " ... Silver: " + playerSilver + " ... Gold: " + playerGold);
+		}
+
+	}
+}
